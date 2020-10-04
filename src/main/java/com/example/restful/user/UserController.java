04cc192de3d14818bc6b36aefe22aad8978d33df
mangojuicedeli <mgkang0317@gmail.com>
2020-10-04 16:23:21 +0900

    User 도메인: POST api 구현. 201 상태코드 예시.

--- src/main/java/com/example/restful/user/UserController.java
+++ src/main/java/com/example/restful/user/UserController.java
@@ -1,10 +1,11 @@
 package com.example.restful.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -16,6 +17,20 @@ public class UserController {
     @GetMapping("/users")
     public List<User> retrieveAll() {
         return userDaoService.findAll();
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        userDaoService.save(user);
+
+        // 서버에서 만들어진 등록된 사용자 id를 응답으로 리턴함으로써, id를 알기 위한 추가적인 요청이 필요없어져 트래픽을 감소시킬 수 있다.
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(retrieveOne(user.getId()).getId())
+                .toUri();
+
+        // 모든 성공한 요청을 200으로 처리하는게 아니라, 경우에 맞게 세분화해야한다.(created = 201)
+        return ResponseEntity.created(location).build();
     }
 
     @GetMapping("/users/{id}")
--- src/main/java/com/example/restful/user/UserDaoService.java
+++ src/main/java/com/example/restful/user/UserDaoService.java
@@ -11,8 +11,8 @@ public class UserDaoService {
 
     // 예제의 편의를 위해 DB의 역할은 List로 대체한다.
     private static List<User> users = new ArrayList<>();
-
     private static int userCount = 3;
+
     static {
         users.add(new User(1, "tester1", new Date()));
         users.add(new User(2, "tester2", new Date()));
@@ -32,11 +32,10 @@ public class UserDaoService {
         return null;
     }
 
-    public User save(User user) {
+    public void save(User user) {
         if (user.getId() == null) {
             user.setId(++userCount);
         }
         users.add(user);
-        return user;
     }
 }
